51ba6f301e0895d5da57091002f69e420e0322ce
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2020-04-26 06:27:50 +0800

    Added models

---
+++ test/services/events.test.ts
@@ -0,0 +1,8 @@
+import app from '../../src/app';
+
+describe('\'events\' service', () => {
+  it('registered the service', () => {
+    const service = app.service('events');
+    expect(service).toBeTruthy();
+  });
+});
