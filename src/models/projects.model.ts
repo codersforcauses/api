130d24a1f5e591d24a0a9a70d535640a5538fba4
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2020-09-18 00:05:01 +0800

    remove matches field

--- src/models/projects.model.ts
+++ src/models/projects.model.ts
@@ -2,13 +2,13 @@
 //
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
-import mongoose from "mongoose";
-import { Application } from "../declarations";
+import mongoose from 'mongoose'
+import { Application } from '../declarations'
 
 export default function (app: Application) {
-  const modelName = "projects";
-  const mongooseClient = app.get("mongooseClient");
-  const { Schema } = mongooseClient;
+  const modelName = 'projects'
+  const mongooseClient = app.get('mongooseClient')
+  const { Schema } = mongooseClient
   const schema = new Schema(
     {
       name: {
@@ -16,21 +16,21 @@ export default function (app: Application) {
         maxLength: 128,
         index: true,
         trim: true,
-        required: true,
+        required: true
       },
       description: {
         type: String,
         maxLength: 2048,
         trim: true,
-        required: true,
+        required: true
       },
       type: [
         {
           type: String,
-          enum: ["web", "mobile_app", "desktop_app"],
-          default: "web",
-          index: true,
-        },
+          enum: ['web', 'mobile_app', 'desktop_app'],
+          default: 'web',
+          index: true
+        }
       ],
       client: [
         {
@@ -39,29 +39,28 @@ export default function (app: Application) {
             maxLength: 128,
             index: true,
             trim: true,
-            required: true,
+            required: true
           },
           description: {
             type: String,
             maxLength: 2048,
             trim: true,
-            required: true,
+            required: true
           },
           email: {
             type: String,
-            match: "^.+@.+\\..+$",
             maxLength: 128,
             index: true,
             trim: true,
             lowercase: true,
-            required: true,
-          },
-        },
+            required: true
+          }
+        }
       ],
       startDate: {
         type: Date,
         required: true,
-        index: true,
+        index: true
       },
       imageLinks: [String],
       impact: [{ type: String, maxLength: 256, trim: true }],
@@ -70,41 +69,41 @@ export default function (app: Application) {
           type: {
             type: String,
             enum: [
-              "github",
-              "gitlab",
-              "bitbucket",
-              "app_store",
-              "play_store",
-              "website",
-            ],
+              'github',
+              'gitlab',
+              'bitbucket',
+              'app_store',
+              'play_store',
+              'website'
+            ]
           },
           link: {
             type: String,
             maxLength: 256,
-            trim: true,
-          },
-        },
+            trim: true
+          }
+        }
       ],
       tech: [
         {
           type: {
             type: String,
             maxLength: 64,
-            index: true,
-          },
-        },
+            index: true
+          }
+        }
       ],
-      users: [{ type: mongoose.Schema.Types.ObjectId }],
+      users: [{ type: mongoose.Schema.Types.ObjectId }]
     },
     {
-      timestamps: true,
+      timestamps: true
     }
-  );
+  )
 
   // This is necessary to avoid model compilation errors in watch mode
   // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
   if (mongooseClient.modelNames().includes(modelName)) {
-    mongooseClient.deleteModel(modelName);
+    mongooseClient.deleteModel(modelName)
   }
-  return mongooseClient.model(modelName, schema);
+  return mongooseClient.model(modelName, schema)
 }
--- src/models/users.model.ts
+++ src/models/users.model.ts
@@ -2,130 +2,128 @@
 //
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
-import mongoose from "mongoose";
-import { Application } from "../declarations";
+import mongoose from 'mongoose'
+import { Application } from '../declarations'
 
 export default function (app: Application) {
-  const modelName = "users";
-  const mongooseClient = app.get("mongooseClient");
-  const { Schema } = mongooseClient;
+  const modelName = 'users'
+  const mongooseClient = app.get('mongooseClient')
+  const { Schema } = mongooseClient
   const schema = new Schema(
     {
       firstName: {
         type: String,
-        match: "^[\\w'\\-,.][^_!¡?÷?¿\\/\\\\+=@#$%ˆ*(){}|~<>;:[\\]]*$",
         maxLength: 64,
         index: true,
         trim: true,
-        required: true,
+        required: true
       },
       lastName: {
         type: String,
-        match: "^[\\w'\\-,.][^_!¡?÷?¿\\/\\\\+=@#$%ˆ*(){}|~<>;:[\\]]*$",
         maxLength: 64,
         index: true,
         trim: true,
-        required: true,
+        required: true
       },
       email: {
         type: String,
-        match: "^.+@.+\\..+$",
+        match: '^.+@.+\\..+$',
         maxLength: 128,
         index: true,
         trim: true,
         lowercase: true,
         required: true,
-        unique: true,
+        unique: true
       },
       gender: {
         type: String,
-        enum: ["female", "male", "other"],
+        enum: ['female', 'male', 'other']
       },
       dob: Date,
       roles: [
         {
           type: String,
           enum: [
-            "member",
-            "president",
-            "vice_president",
-            "secretary",
-            "treasurer",
-            "tech_lead",
-            "marketing_officer",
-            "ocm",
-            "first_year_rep",
+            'member',
+            'president',
+            'vice_president',
+            'secretary',
+            'treasurer',
+            'tech_lead',
+            'marketing_officer',
+            'ocm',
+            'first_year_rep'
           ],
-          index: true,
-        },
+          index: true
+        }
       ],
       socials: [
         {
           type: {
             type: String,
             enum: [
-              "github",
-              "gitlab",
-              "bitbucket",
-              "facebook",
-              "instagram",
-              "twitter",
-              "linkedin",
-              "foursquare",
-              "wechat",
-              "website",
-              "snapchat",
-            ],
+              'github',
+              'gitlab',
+              'bitbucket',
+              'facebook',
+              'instagram',
+              'twitter',
+              'linkedin',
+              'foursquare',
+              'wechat',
+              'website',
+              'snapchat'
+            ]
           },
           link: {
             type: String,
             maxLength: 128,
-            trim: true,
-          },
-        },
+            trim: true
+          }
+        }
       ],
       projects: [
         {
-          type: mongoose.Schema.Types.ObjectId,
-        },
+          type: mongoose.Schema.Types.ObjectId
+        }
       ],
       events: [
         {
-          type: mongoose.Schema.Types.ObjectId,
-        },
+          type: mongoose.Schema.Types.ObjectId
+        }
       ],
       profileImage: String,
       isFinancialMember: {
         type: Boolean,
-        default: false,
+        default: false
       },
       services: [
         {
           type: {
             type: String,
-            enum: ["aws_cognito", "stripe"],
+            enum: ['aws_cognito', 'stripe']
           },
           ref: {
             type: String,
-            maxLength: 1024,
+            maxLength: 1024
           },
           updatedAt: Date,
-          data: {},
-        },
-      ],
+          data: {}
+        }
+      ]
     },
     {
-      timestamps: true,
+      timestamps: true
     },
     {
-      timestamps: true,
+      timestamps: true
     }
-  );
+  )
 
   // This is necessary to avoid model compilation errors in watch mode
   // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
   if (mongooseClient.modelNames().includes(modelName)) {
-    mongooseClient.deleteModel(modelName);
+    mongooseClient.deleteModel(modelName)
   }
-  return mongooseClient.model(modelName, schema);
+  return mongooseClient.model(modelName, schema)
 }
